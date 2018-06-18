ecddcf3b76c1dbf859fbb640f860b41672eee312
liqi <1286533256@qq.com>
2018-06-18 17:31:19 +0800

    ji

--- mainGraph.cpp
+++ mainGraph.cpp
@@ -1,12 +1,11 @@
 //
 // Created by liqi on 18-6-8.
-//
-#include "graph/ALG_graph.h"
-#include "graph/BFS.h"
-int main(){
-    Graph graph;
-    CreatGraph(graph);
-    BFS(graph,1);
-    cout<<NextNeighbor(graph,3,2)<<endl;
-    cout<<FirstNeighbor(graph,4)<<endl;
-}
+////
+//#include "graph/ALG_graph.h"
+//#include "graph/DFS_not_recursion.h"
+//#include "graph/BFS.h"
+//int main(){
+//    Graph graph;
+//    CreatGraph(graph);
+//    DFS(graph,1);
+//}
